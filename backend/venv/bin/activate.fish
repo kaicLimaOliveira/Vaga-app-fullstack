581705b023f315ac83cf0e4e971645d6cb4b3cfc
kaicLimaOliveira <kaic10@outlook.com>
2022-02-25 17:53:51 -0300

    commit

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sistema/Documentos/Projetos Kaic/Vagas-app-fullstack/back/venv"
+set -gx VIRTUAL_ENV "/home/sistema/Documentos/Projetos Kaic/Vagas-app-fullstack/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
