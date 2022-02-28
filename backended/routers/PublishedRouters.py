b1f4020cd28b1c48657a610128a1af5eb1f78abc
Kaic de Lima Oliveira <kaic10@outlook.com>
2022-02-28 08:30:05 -0300

    commit

---
+++ backended/routers/PublishedRouters.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request
+from controllers.PublishersController import Publishers
+
+pages = Blueprint('', __name__, url_prefix='/')
+
+@pages.route('home', methods=['GET'])
+def index():
+    return Publishers().index(request)
+
+@pages.route('new_vacancy', methods=['GET', 'POST'])
+def register_vacancy():
+    return Publishers().new_vacancy(request)
