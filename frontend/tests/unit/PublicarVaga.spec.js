a13a5d3b8d94946cd5669c315c3def30c1d4c709
kaicLimaOliveira <kaic10@outlook.com>
2022-03-28 12:11:00 -0300

    Adicionando testes com jest; Removendo pastas ociosas

--- frontend/tests/unit/PublicarVaga.spec.js
+++ frontend/tests/unit/PublicarVaga.spec.js
@@ -1,7 +1,5 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import PublicarVaga from '@/components/views/PublicarVaga.vue'
-
-jest.mock('../../src/components/views/PublicarVaga.vue')
 
 describe('PublicarVaga.vue', () => {
   test('Simulação de envio do Form', async () => {
@@ -10,42 +8,40 @@ describe('PublicarVaga.vue', () => {
     // Input de título da vaga
     const title = wrapper.find('#title')
     await title.setValue('Desenvolvedor de Testes')
-
-    title.element.value = value
     title.trigger('input')
+
     expect(title.element.value).toBe('Desenvolvedor de Testes')
 
     // Input de descrição da vaga
-    // const description = wrapper.find('#description')
-    // await description.setValue('Saber usa Jest em aplicações VueJs')
+    const description = wrapper.find('#description')
+    await description.setValue('Saber usa Jest em aplicações VueJs')
+    description.trigger('input')
 
-    // expect(description.element.value).toBe('Saber usa Jest em aplicações VueJs')
+    expect(description.element.value).toBe('Saber usa Jest em aplicações VueJs')
 
-    // // Input do salário da vaga
-    // const salary = wrapper.find('#salary')
-    // salary.setValue(5000)
+    // Input do salário da vaga
+    const salary = wrapper.find('#salary')
+    // await salary.setValue(5000)
+    salary.element.value = 5000
+    salary.trigger('input')
 
-    // expect(salary.element.value).toBe(5000)
+    expect(salary.element.value).toBe('5000')
 
     // // Input da modalidade da vaga
-    // const modality = wrapper.find('#modality')
-    // modality.setValue('Home Office')
+    const modality = wrapper.find('#modality').element
+    modality.value = 2
+    modality.dispatchEvent(new Event('change'))
 
-    // expect(modality.element.value).toBe('Home Office')
+    expect(modality.value).toEqual('2')
 
     // // Input do tipo da vaga
-    // const type = wrapper.find('#type')
-    // type.setValue('PJ')
+    const type = wrapper.find('#type')
+    await type.setValue(2)
 
-    // expect(type.element.value).toBe('PJ')
+    type.trigger('change')
 
-
-    // await wrapper.vm.nextTick()
-  })
-
-  test('Jest doc', () => {
-    const wrapper = mount(PublicarVaga)
-    expect(wrapper.element)
+    expect(type.element.value).toEqual('2')
 
   })
+
 })
